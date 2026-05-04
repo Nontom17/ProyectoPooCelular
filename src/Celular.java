d49e33d64c3b09d79f7d700a4047013b462ae5ed
Tomas <tomitomnu@gmail.com>
2026-05-04 14:24:35 -0400

    primer commit

---
+++ src/Celular.java
@@ -0,0 +1,24 @@
+public class Celular {
+    //atributos
+
+    private String marca;
+    private String modelo;
+    private String memoria;
+    private String ram;
+    private String conectividad;
+
+    public Celular(String marca, String modelo, String memoria, String ram, String conectividad){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.memoria = memoria;
+        this.ram = ram;
+        this.conectividad = conectividad;
+
+    }
+
+
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Celular celular1 = new Celular("Xiaomi", "Redmi 10A", "32GB","2GB","4G");
+       /* celular1.marca = "Xiaomi";
+        celular1.modelo = "Redmi 10A";
+        celular1.memoria = "32GB";
+        celular1.ram = "2GB";
+        celular1.conectividad = "4G";
+        System.out.println(celular1.marca);
+        System.out.println(celular1.modelo);
+        System.out.println(celular1.memoria);
+        System.out.println(celular1.ram);
+        */
+
+
+
+
+    }
+}
